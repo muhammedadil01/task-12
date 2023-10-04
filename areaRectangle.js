6cf8ad9415d51c07e4724b95268a2a9aa20fa348
muhammedadil01 <muhammedadil932@gmail.com>
2023-10-04 09:18:40 +0530

    first commit

---
+++ areaRectangle.js
@@ -0,0 +1,12 @@
+
+var num1 = 5
+var num2 = 5
+
+const result = areaRectangle(num1,num2)
+
+
+function areaRectangle(a,b){
+   return  a*b
+ }
+
+ console.log(result);
---
+++ areaSquare.js
@@ -0,0 +1,11 @@
+
+var value = 4
+ 
+
+function areaSquare(index){
+
+    let result = index*index
+    return result
+}
+
+console.log(areaSquare(value));
